c140b71d5097d637e5f932469123838e659145cc
peterelia22 <peterelia914@gmail.com>
2024-04-28 20:33:40 +0300

    splash page

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const test = 'assets/images/logo-white.png';
---
+++ lib/screens/splash.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:spellable/constants.dart';
+import 'package:spellable/screens/home.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset('assets/images/logo-white.png'),
+          const SizedBox(
+            height: 15,
+          ),
+          SlidingAnimation(slidingAnimation: slidingAnimation),
+        ],
+      ),
+    );
+  }
+
+  void initAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: const Offset(0, -6))
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) {
+          return HomePage();
+        }),
+      );
+    });
+  }
+}
+
+class SlidingAnimation extends StatelessWidget {
+  const SlidingAnimation({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, child) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Learn With Spellable',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    );
+  }
+}
